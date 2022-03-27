afd1502d98dc70da9231a62b9d4ee05f56f7fa15
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-28 00:27:27 +0700

    don't render add link if already in add page

--- app/routes/__app.tsx
+++ app/routes/__app.tsx
@@ -1,4 +1,4 @@
-import { Form, Link, Outlet, redirect } from 'remix'
+import { Form, Link, Outlet, redirect, useLocation } from 'remix'
 import type { LoaderFunction } from 'remix'
 import Tabs from '~/components/Tabs'
 import { requireUser, setUser } from '~/utils/auth.server'
@@ -43,6 +43,10 @@ export const loader: LoaderFunction = async ({ request }) => {
 }
 
 export default function AppLayout() {
+  const { pathname } = useLocation()
+
+  const isAddPage = pathname === '/add'
+
   return (
     <div className="min-h-full py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-3xl">
@@ -52,13 +56,16 @@ export default function AppLayout() {
 
         <Tabs />
 
-        <div className="mt-8 flex gap-4">
-          <Link
-            to="/add"
-            className="px-4 py-2 text-center bg-primary-9 hover:bg-primary-10 flex-1 text-white rounded-md"
-          >
-            Add new note
-          </Link>
+        <div className="mt-8 flex gap-4 justify-end">
+          {isAddPage ? null : (
+            <Link
+              to="/add"
+              className="px-4 py-2 text-center bg-primary-9 hover:bg-primary-10 flex-1 text-white rounded-md"
+            >
+              Add new note
+            </Link>
+          )}
+
           <Form action="/logout" method="post">
             <button className="px-4 py-2 bg-primary-3 hover:bg-primary-4 active:bg-primary-5 text-primary-11 hover:text-primary-12 rounded-md">
               Logout
