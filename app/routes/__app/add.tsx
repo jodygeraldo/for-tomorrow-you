48ba122089073fa41d4a624d17739c166551411d
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-28 01:21:35 +0700

    add some transition

--- app/routes/__app/add.tsx
+++ app/routes/__app/add.tsx
@@ -1,9 +1,10 @@
-import { Form, redirect } from 'remix'
+import { Form, redirect, useTransition } from 'remix'
 import type { ActionFunction } from 'remix'
 import { sb } from '~/utils/supabase.server'
 import { requireUser } from '~/utils/auth.server'
 import { Notes } from '~/utils/types.server'
 import invariant from 'tiny-invariant'
+import { useEffect, useRef } from 'react'
 
 export const action: ActionFunction = async ({ request }) => {
   const user = await requireUser(request)
@@ -33,27 +34,41 @@ export const action: ActionFunction = async ({ request }) => {
     return null
   }
 
-  return redirect('/')
+  return null
 }
 
 export default function () {
+  const { state } = useTransition()
+  const busy = state === 'submitting'
+  const ref = useRef<HTMLFormElement>(null)
+
+  useEffect(() => {
+    if (state === 'loading') {
+      ref.current?.reset()
+    }
+  }, [state])
+
   return (
-    <Form className="mt-6" method="post">
-      <div>
-        <label htmlFor="note" className="sr-only">
-          Note
-        </label>
-        <textarea
-          name="note"
-          id="note"
-          rows={6}
-          className="appearance-none block w-full px-3 py-2 border border-gray-7 rounded-md shadow-sm placeholder-gray-11 focus:outline-none focus:ring-primary-8 focus:border-primary-8 sm:text-sm bg-gray-2 text-gray-12 disabled:bg-gray-7 disabled:cursor-not-allowed"
-          placeholder="Your note"
-        />
-      </div>
-      <button className="mt-4 px-4 py-2 text-center bg-primary-9 hover:bg-primary-10 flex-1 text-white rounded-md w-full">
-        Add new note
-      </button>
+    <Form ref={ref} className="mt-6" method="post">
+      <fieldset disabled={busy}>
+        <div>
+          <label htmlFor="note" className="sr-only">
+            Note
+          </label>
+          <textarea
+            name="note"
+            id="note"
+            rows={6}
+            className="appearance-none block w-full px-3 py-2 border border-gray-7 rounded-md shadow-sm placeholder-gray-11 focus:outline-none focus:ring-primary-8 focus:border-primary-8 sm:text-sm bg-gray-2 text-gray-12 disabled:bg-gray-7 disabled:cursor-not-allowed"
+            placeholder="Your note"
+            required
+            minLength={3}
+          />
+        </div>
+        <button className="mt-4 px-4 py-2 text-center bg-primary-9 hover:bg-primary-10 flex-1 text-white rounded-md w-full disabled:bg-primary-6">
+          {busy ? 'Adding note...' : 'Add new note'}
+        </button>
+      </fieldset>
     </Form>
   )
 }
