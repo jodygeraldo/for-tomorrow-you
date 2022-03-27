d6e0987fa926e1b622afe6cd6cbe85404c16a744
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-27 23:31:55 +0700

    add add notes link and logout button

--- app/routes/__app.tsx
+++ app/routes/__app.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'remix'
+import { Form, Link, Outlet } from 'remix'
 import Tabs from '~/components/Tabs'
 
 export default function AppLayout() {
@@ -10,6 +10,20 @@ export default function AppLayout() {
         </h1>
 
         <Tabs />
+
+        <div className="mt-8 flex gap-4">
+          <Link
+            to="/add"
+            className="px-4 py-2 text-center bg-primary-9 hover:bg-primary-10 flex-1 text-white rounded-md"
+          >
+            Add new note
+          </Link>
+          <Form action="/logout" method="post">
+            <button className="px-4 py-2 bg-primary-3 hover:bg-primary-4 active:bg-primary-5 text-primary-11 hover:text-primary-12 rounded-md">
+              Logout
+            </button>
+          </Form>
+        </div>
 
         <Outlet />
       </div>
