1f6695b839b779c78680a6899824c34a21689113
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-28 00:53:17 +0700

    feat: add logs

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,13 +1,14 @@
-export default function Card({ children }: { children: React.ReactNode }) {
+export default function Card({
+  children,
+  note,
+}: {
+  children: React.ReactNode
+  note: string
+}) {
   return (
     <div className="space-y-4 mt-8">
       <div className="flex justify-between bg-gray-3 border-2 border-gray-6 rounded-md px-4 py-2 gap-8">
-        <p className="text-gray-12 text-lg">
-          Lorem ipsum Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Temporibus earum non voluptas quod. Nihil nobis eaque aperiam labore
-          mollitia aliquam in pariatur natus, reprehenderit ratione tempora rem
-          laudantium assumenda eum?
-        </p>
+        <p className="text-gray-12 text-lg">{note}</p>
 
         {children}
       </div>
--- app/routes/__app/logs.tsx
+++ app/routes/__app/logs.tsx
@@ -1,8 +1,17 @@
 import Card from '~/components/Card'
-import { redirect } from 'remix'
+import { json, redirect, useLoaderData } from 'remix'
 import type { LoaderFunction } from 'remix'
 import { requireUser } from '~/utils/auth.server'
+import invariant from 'tiny-invariant'
+import { Notes } from '~/utils/types.server'
+import { sb } from '~/utils/supabase.server'
 
+type LoaderData = {
+  id: number
+  note: string
+  expiresAt: string
+  expiresAtFormatted: string
+}[]
 export const loader: LoaderFunction = async ({ request }) => {
   const user = await requireUser(request)
 
@@ -10,13 +19,47 @@ export const loader: LoaderFunction = async ({ request }) => {
     return redirect('/login')
   }
 
-  return null
+  const { error, data } = await sb.from<Notes>('notes').select('*')
+
+  if (error) {
+    console.log(error)
+    return json({ error: 'error' })
+  }
+
+  invariant(data, 'data should be an array')
+
+  const now = new Date().getTime()
+
+  const notesMap = data.filter((note) => note.expires_at < now)
+  console.log(notesMap)
+
+  const notes = notesMap.map((note) => {
+    return {
+      id: note.id,
+      note: note.notes,
+      expiresAt: `${new Date(note.expires_at)}`,
+      expiresAtFormatted: Intl.DateTimeFormat(undefined, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      }).format(new Date(note.expires_at)),
+    }
+  })
+
+  return json<LoaderData>(notes, 200)
 }
 
 export default function LogPage() {
-  return (
-    <Card>
-      <p className="text-gray-11 text-sm min-w-fit">END DATE</p>
+  const notes = useLoaderData<LoaderData>()
+
+  return notes.map((note) => (
+    <Card key={note.id} note={note.note}>
+      <time
+        className="text-gray-11 text-sm min-w-fit"
+        dateTime={note.expiresAt}
+      >
+        {note.expiresAtFormatted}
+      </time>
     </Card>
-  )
+  ))
 }
