fce96479ccc9dbaacbaea0a10b0dca63ed4cf719
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-27 23:06:52 +0700

    add route guard

--- app/routes/__app.tsx
+++ app/routes/__app.tsx
@@ -1,5 +1,17 @@
-import { Outlet } from 'remix'
+import { Outlet, redirect } from 'remix'
+import type { LoaderFunction } from 'remix'
 import Tabs from '~/components/Tabs'
+import { requireUser } from '~/utils/auth.server'
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await requireUser(request)
+
+  if (!user) {
+    return redirect('/login')
+  }
+
+  return null
+}
 
 export default function AppLayout() {
   return (
