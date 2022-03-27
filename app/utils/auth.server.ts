d3e31d6b370e9d9277f3f3962ad0da59531fe413
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-27 23:05:47 +0700

    add cookie and helper function

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -3,6 +3,13 @@ import invariant from 'tiny-invariant'
 
 invariant(process.env.AUTH_COOKIE_SECRET, 'AUTH_COOKIE_SECRET is not set')
 
+export interface UserData {
+  id: string
+  accessToken: string
+  refreshToken: string
+  expiresIn: number
+}
+
 export const { getSession, commitSession, destroySession } =
   createCookieSessionStorage({
     cookie: {
@@ -14,3 +21,19 @@ export const { getSession, commitSession, destroySession } =
       secure: process.env.NODE_ENV === 'production',
     },
   })
+
+export async function getUserId(request: Request) {
+  const session = await getSession(request.headers.get('Cookie'))
+
+  const userId: UserData | undefined = session.get('user')
+
+  return userId?.id
+}
+
+export async function requireUser(request: Request) {
+  const session = await getSession(request.headers.get('Cookie'))
+
+  const user: UserData | undefined = session.get('user')
+
+  return user
+}
