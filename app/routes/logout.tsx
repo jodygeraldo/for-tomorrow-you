61107db849f8df09c1a4a08a272e23d540c687b9
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-27 23:33:41 +0700

    add logout route

---
+++ app/routes/logout.tsx
@@ -0,0 +1,21 @@
+import { redirect } from 'remix'
+import type { LoaderFunction, ActionFunction } from 'remix'
+import { destroySession, getSession } from '~/utils/auth.server'
+
+export const action: ActionFunction = async ({ request }) => {
+  const session = await getSession(request.headers.get('Cookie'))
+
+  return redirect('/login', {
+    headers: {
+      'Set-Cookie': await destroySession(session),
+    },
+  })
+}
+
+export const loader: LoaderFunction = () => {
+  return redirect('/')
+}
+
+export default function LogoutPage() {
+  return null
+}
